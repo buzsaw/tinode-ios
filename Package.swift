7adba49d4c1be60d76931e88ced8c6aea15f73fd
jeremyosterhoudt <jeremyosterhoudt@yahoo.com>
2025-11-06 18:05:34 -0800

    Refactor Package.swift by removing comments

--- Package.swift
+++ Package.swift
@@ -5,34 +5,15 @@ let package = Package(
     name: "TinodeiOS",
     defaultLocalization: "en",
     platforms: [
-        .iOS(.v13) // bump to v14+ if you need newer APIs
+        .iOS(.v13)
     ],
     products: [
-        // This is the only public product users will add to their apps.
         .library(name: "TinodeSDK", targets: ["TinodeSDK"])
-    ],
-    dependencies: [
-        // Keep empty unless the *SDK itself* needs external packages.
-        // App-level deps like Firebase, WebRTC, MobileVLCKit, etc. stay in the app,
-        // not in the SDK package.
     ],
     targets: [
         .target(
             name: "TinodeSDK",
-            path: "TinodeSDK",
-            exclude: [
-                // Add any non-source items in this folder you don’t want compiled.
-                // Examples (uncomment if present):
-                // "Info.plist"
-            ],
-            resources: [
-                // Declare resources if the SDK ships bundles/strings/images.
-                // .process("Resources")
-            ],
-            swiftSettings: [
-                // Enable stricter warnings if you like:
-                // .enableExperimentalFeature("StrictConcurrency")
-            ]
+            path: "TinodeSDK"
         ),
         .testTarget(
             name: "TinodeSDKTests",
